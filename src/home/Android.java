5af7ce3a6dbf1ba2bdca641a9eeac7b094f2220a
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-04-29 00:28:57 -0700

    interface

---
+++ src/home/Android.java
@@ -0,0 +1,12 @@
+package home;
+
+public interface Android{
+
+    public void showWeather();
+    public void playVideos();
+    public void playMusic();
+    public void playMovies();
+
+    
+
+}
